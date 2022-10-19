48f00dfb0053632517f3787df7819776973082f6
DaeguDeveloper <umjaejeong@gmail.com>
2022-10-19 14:25:31 +0900

    회원 수정 /회원 조회/ 샘플데이터 코드추가

--- src/main/java/jpabook/jpashop/api/MemberApiController.java
+++ src/main/java/jpabook/jpashop/api/MemberApiController.java
@@ -10,6 +10,8 @@ import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import javax.validation.constraints.NotEmpty;
+import java.util.List;
+import java.util.stream.Collectors;
 
 // @Controller @ResponseBody 두개를 합친것이 아래의 RestController이다.
 @RestController
@@ -18,10 +20,38 @@ public class MemberApiController {
 
     private final MemberService memberService;
 
+    @GetMapping("/api/v1/members")
+    public List<Member> membersV1(){
+        return memberService.findMembers();
+    }
 
+
+    //Result와 같이 클래스로 감싸줘야 한다.
+    //만약, V1과 같이 List로 바로 반환 해버리면 유연성이 떨어지므로 스펙을 확장시킬 수 없다.
+    @GetMapping("/api/v2/members")
+    public Result membersV2(){
+        List<Member> findMembers = memberService.findMembers();
+        List<MemberDto> collect = findMembers.stream()
+                .map(m -> new MemberDto(m.getName()))
+                .collect(Collectors.toList());
+        return new Result(collect);
+    }
+
+    @Data
+    @AllArgsConstructor
+    static class Result<T>{
+        private T data;
+    }
+
+    @Data
+    @AllArgsConstructor
+    static class MemberDto{
+        private String name;
+    }
     @PostMapping("/api/v1/members")
     //Json으로 받은 멤버를 Member.class의 매개변수 member에 다 넣어 반환해 준다.
     public CreateMemberResponse saveMemberV1(@RequestBody @Valid Member member) {  //javaX Validation이 자동으로 됨
+
         Long join = memberService.join(member);
         return new CreateMemberResponse(join);
     }
